6adce67fcc804869d24ef43419ad772e53e7b2e4
bob <zhuhaibobb@163.com>
2015-05-05 23:38:59 +0800

    Initial Commit

---
+++ bbnote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bbnote
+//
+//  Created by bob on 5/5/15.
+//  Copyright (c) 2015 bob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
